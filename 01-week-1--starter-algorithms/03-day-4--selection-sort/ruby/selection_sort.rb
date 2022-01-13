b0de822ab6b2f2a16168dd1369bc4b634ca0d4e6
Omar <omar.branez@gmail.com>
2022-01-12 20:03:30 -0500

    selection solve

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
@@ -1,5 +1,17 @@
 def selection_sort(arr)
   # type your code in here
+  (arr.length - 1).times do |element_index_to_check|
+    current_min_index = element_index_to_check + 1
+    element_index_to_check.upto(arr.length - 1) do |left_num_index|
+      if arr[left_num_index] < arr[current_min_index]
+        current_min_index = left_num_index
+      end
+    end
+    temp_left_num = arr[current_min_index]
+    arr[current_min_index] = arr[element_index_to_check]
+    arr[element_index_to_check] = temp_left_num
+  end
+    arr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -18,4 +30,11 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+# arr length - 1 ignores zero. this is how many times we have to run this iteration
+# assume the second number is the minimum and store it as a temporary variable
+# check from the 0 index and work to the right
+# if the latter number is less than the former number
+# set that latter number as the current minimum between the two
+# store the value of whatever was determined to be the current minimum
+# 
 # And a written explanation of your solution
