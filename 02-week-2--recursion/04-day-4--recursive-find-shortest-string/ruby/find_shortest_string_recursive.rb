5528db4b2fcf6cca4d396119511500a580b91ce6
Omar <omar.branez@gmail.com>
2022-01-13 18:31:36 -0500

    solve shortest string recursive

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
@@ -1,5 +1,11 @@
 def find_shortest_string_recursive(arr)
   # type your code in here
+  if arr.length == 1
+    return arr[0]
+  end
+    # puts(arr[1..-1])
+    testString = find_shortest_string_recursive(arr[1..-1])
+    arr[0].length <= testString.length ? arr[0] : testString
 end
 
 if __FILE__ == $PROGRAM_NAME
