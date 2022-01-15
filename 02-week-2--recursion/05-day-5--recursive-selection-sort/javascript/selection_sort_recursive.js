5ae4afddbd4f5dab0d176596145bc746d22f1dff
Omar <omar.branez@gmail.com>
2022-01-14 22:51:09 -0500

    solve selection sort recursive js

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,23 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  // output a new array from the input
+  // specifically wants an empty array to be returned if the array is empty
+  if (arr.length == 0){
+    return []
+  }
+  // math min returns the smallest element in a given input, but not as an array
+  let minimum = Math.min(...arr)
+  // get that index number from the input array
+  let index = arr.indexOf(minimum)
+  // once you have that number, remove it from the input array
+  arr.splice(index, 1)
+  
+  // test will be our output array
+  let test = selectionSortRecursive(arr)
+  // minimum gets moved into the new array
+  test.unshift(minimum)
+  // keep running test until the arr array is empty
+  return test
 }
 
 if (require.main === module) {
