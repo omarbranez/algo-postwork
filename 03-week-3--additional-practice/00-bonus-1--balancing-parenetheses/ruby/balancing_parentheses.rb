cdcadeba6d1d1e684afc3c9bbd9ba72423a6f2e5
Omar <omar.branez@gmail.com>
2022-01-17 23:57:58 -0500

    solve balancing ruby

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
@@ -1,5 +1,21 @@
 def balancing_parentheses(string)
   # type your code in here
+  end_count = 0
+  opening_count = 0
+  string.chars.each do |char|
+    if char == '('
+      opening_count += 1
+      next
+    end
+
+    if opening_count > 0 
+      opening_count -= 1
+    else
+      end_count += 1
+    end
+  end
+
+  end_count + opening_count
 end
 
 if __FILE__ == $PROGRAM_NAME
